e4a737d5f01cbd19bc1b8770ab6701f52296eeb3
Vinilazza <108952051+Vinilazza@users.noreply.github.com>
2023-10-07 16:16:57 -0300

    up

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
   const form = document.getElementById("formulario");
 
   form.addEventListener("submit", function (e) {
-    e.preventDefault(); // Impede o envio padrão do formulário
+    e.preventDefault(); 
 
     const nome = document.getElementById("nome").value;
     const matricula = document.getElementById("matricula").value;
@@ -10,9 +10,6 @@ document.addEventListener("DOMContentLoaded", function () {
     const email = document.getElementById("email").value;
     const turma = document.getElementById("turma").value;
 
-    
-
-    // Simulação de envio dos dados para o servidor (substitua pelo seu código real)
     const dados = {
       nome: nome,
       matricula: matricula,
@@ -21,8 +18,7 @@ document.addEventListener("DOMContentLoaded", function () {
       turma: turma,
     };
 
-    // Você pode substituir este URL pelo endpoint real onde deseja enviar os dados
-    const url = "https://jsonplaceholder.typicode.com/posts";
+    const url = "http://jkorpela.fi/cgi-bin/echo.cgi";
 
     fetch(url, {
       method: "POST",
@@ -35,10 +31,9 @@ document.addEventListener("DOMContentLoaded", function () {
       .then((data) => {
         console.log("Resposta do servidor:", data);
 
-        // Verifica se a inscrição foi bem-sucedida com base na resposta do servidor
         if (data) {
           document.getElementById("mensagem").textContent = "Inscrição efetuada com sucesso.";
-          form.reset(); // Limpa o formulário após o envio bem-sucedido
+          form.reset();
         } else {
           alert("Ocorreu um erro ao processar a inscrição. Por favor, tente novamente.");
         }
